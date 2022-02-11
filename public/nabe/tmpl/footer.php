9fd8fff98a40c38c18268c0d2de17232c7deaf36
LEE_NARA__ <dlskfko66@gmail.com>
2022-02-11 13:35:19 +0900

    불필요한 파일 삭제

--- public/nabe/tmpl/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once $_SERVER["DOCUMENT_ROOT"]."/site/common/tmpl/footer.php";
-include_once "footer_site.php";
-?>
--- public/nabe/tmpl/submenu.php
+++
@@ -1,3 +0,0 @@
-<?php
-include_once $_SERVER["DOCUMENT_ROOT"]."/site/common/tmpl/submenu.php";
-?>
